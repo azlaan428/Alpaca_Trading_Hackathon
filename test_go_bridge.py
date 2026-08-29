86b43dba6eeb3891e1154c5f2983cb309a3726fc
azlaan428 <azlaanmohammad95@gmail.com>
2026-08-29 22:58:34 +0500

    remove scratch test file for Go/Python interop check

--- test_go_bridge.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-response = requests.get("http://localhost:8080/account")
-data = response.json()
-
-print("Account status via Go service:", data["status"])
-print("Buying power via Go service:", data["buying_power"])
